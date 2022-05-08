66e1ac6e91cd9c6615ff07960c0640d95e26a5da
Niamul Hasan Zead <zead.ece2014@gmail.com>
2022-05-08 08:19:15 +0600

    private route is checked

--- src/Pages/Home/Combo/ComboBooking.js
+++ src/Pages/Home/Combo/ComboBooking.js
@@ -1,35 +1,46 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import auth from '../../../firebase.init';
 
 const ComboBooking = ({ offer }) => {
 
+    const navigate = useNavigate();
+
     const [user] = useAuthState(auth);
 
+    // if (!user) {
+    //     navigate('/login')
+    // }
+
     const handleBookNow = () => {
-        const bookingCombo = {
-            uName: user.displayName,
-            email: user.email,
-            iName: offer.name,
-            img: offer.img,
-            quantity: offer.offerPrice
+        if (user) {
+            const bookingCombo = {
+                uName: user.displayName,
+                email: user.email,
+                iName: offer.name,
+                img: offer.img,
+                quantity: offer.offerPrice
 
+            }
+            fetch("https://serene-coast-39945.herokuapp.com/bookings", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(bookingCombo)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                });
+            toast('Your Booking is placed');
         }
-        fetch("https://serene-coast-39945.herokuapp.com/bookings", {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(bookingCombo)
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-            });
-        toast('Your Booking is placed')
-
+        if (!user) {
+            toast('Please login First')
+        }
     }
     return (
         <div>
